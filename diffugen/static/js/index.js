f37296bbc6b8120ddd4a2f82da3340070bd2b0c2
Michael Shenoda <michael@mshenoda.com>
2023-09-08 09:45:22 -0500

    Update index.js

--- diffugen/static/js/index.js
+++ diffugen/static/js/index.js
@@ -2,12 +2,12 @@ window.HELP_IMPROVE_VIDEOJS = false;
 
 var NUM_INTERP_FRAMES = 240;
 
-function setInterpolationImage(i) {
-  var image = interp_images[i];
-  image.ondragstart = function() { return false; };
-  image.oncontextmenu = function() { return false; };
-  $('#interpolation-image-wrapper').empty().append(image);
-}
+// function setInterpolationImage(i) {
+//   var image = interp_images[i];
+//   image.ondragstart = function() { return false; };
+//   image.oncontextmenu = function() { return false; };
+//   $('#interpolation-image-wrapper').empty().append(image);
+// }
 
 
 $(document).ready(function() {
@@ -19,41 +19,41 @@ $(document).ready(function() {
 
     });
 
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 3,
-			loop: true,
-			infinite: true,
-			autoplay: false,
-			autoplaySpeed: 3000,
-    }
+  //   var options = {
+		// 	slidesToScroll: 1,
+		// 	slidesToShow: 3,
+		// 	loop: true,
+		// 	infinite: true,
+		// 	autoplay: false,
+		// 	autoplaySpeed: 3000,
+  //   }
 
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
+		// // Initialize all div with carousel class
+  //   var carousels = bulmaCarousel.attach('.carousel', options);
 
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
-    }
+  //   // Loop on each carousel initialized
+  //   for(var i = 0; i < carousels.length; i++) {
+  //   	// Add listener to  event
+  //   	carousels[i].on('before:show', state => {
+  //   		console.log(state);
+  //   	});
+  //   }
 
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
-    }
+  //   // Access to bulmaCarousel instance of an element
+  //   var element = document.querySelector('#my-element');
+  //   if (element && element.bulmaCarousel) {
+  //   	// bulmaCarousel instance is available as element.bulmaCarousel
+  //   	element.bulmaCarousel.on('before-show', function(state) {
+  //   		console.log(state);
+  //   	});
+  //   }
 
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
-    });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+    // $('#interpolation-slider').on('input', function(event) {
+    //   setInterpolationImage(this.value);
+    // });
+    // setInterpolationImage(0);
+    // $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
 
-    bulmaSlider.attach();
+    // bulmaSlider.attach();
 
 })
